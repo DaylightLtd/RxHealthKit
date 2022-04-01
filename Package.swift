c2773d1f04de82f5eec4a90eb5f51e76fe6e3d9d
skipp <chanchanakoedtho@gmail.com>
2022-04-01 16:39:16 +0700

    Add support swift package

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RxHealthKit",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "RxHealthKit",
+            targets: ["RxHealthKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMinor(from: "6.5.0"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "RxHealthKit",
+            dependencies: ["RxSwift"]),
+        .testTarget(
+            name: "RxHealthKitTests",
+            dependencies: ["RxHealthKit"]),
+    ]
+)
